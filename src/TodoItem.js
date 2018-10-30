cf59f4990634e9d656a9274581b94d3eec06edf1
viviScript <863628979@qq.com>
2018-10-30 23:31:01 +0800

    代码优化

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 class TodoItem extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this)
     }
     handleClick () {
-        this.props.deteleItem(this.props.index)
+        const {deteleItem, index} = this.props;
+        // this.props.deteleItem(this.props.index)
+        deteleItem(index);
+    }
+    // 一个组件要从父组建接收参数
+    // 只要父组建的render函数被重新执行了，子组建的生命周期就会执行
+    // 如果这个组件第一次存在父组建中，不会执行
+    // 如果这个组件之前已经存在父组件中，就会执行
+    componentWillReceiveProps () {
+        console.log('child componentWillReceiveProps')
+    }
+    // 当组件被销毁时，会执行
+    componentWillUnmount () {
+        console.log('child componentWillUnmount')
     }
     render() {
-        const { content } = this.props;
+        const { content, index } = this.props;
         return (
             <div className='list-item'>
-                {content}
+                {index}-{content}
                 <span className='delete' onClick={this.handleClick}>X</span>
             </div>
         )
     }
 }
 
+// 传值校验
+// isRequired 必须传
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    deteleItem: PropTypes.func,
+    index: PropTypes.number
+}
+// 默认传值
+TodoItem.defaultProps = {
+    test: 'hello world'
+}
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -8,6 +8,7 @@ class TodoList extends Component {
             inputValue: '',
             list: []
         }
+        console.log(props)
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleButtonClick = this.handleButtonClick.bind(this);
         this.handleItemDetele = this.handleItemDetele.bind(this);
@@ -15,6 +16,7 @@ class TodoList extends Component {
         this.handleEnterKey = this.handleEnterKey.bind(this);
     }
     render() {
+        console.log('render');
         return (
             <Fragment>
                 <div className='wrapper'>
@@ -25,6 +27,7 @@ class TodoList extends Component {
                     <label className='input-label' htmlFor='insertArea'>输入内容:</label>
                     <input className='input' id='insertArea' value={this.state.inputValue}
                     onChange={this.handleInputChange}
+                    ref={(input) => {this.input = input}}
                     />
                     <button className='input-submit' onClick={this.handleButtonClick}>提交</button>
                 </div>
@@ -36,14 +39,37 @@ class TodoList extends Component {
             </Fragment>
         )
     }
-    componentDidMount () {
-        document.addEventListener("keydown",this.handleEnterKey);
-    }
+    // 在组件即将被挂载到页面的时刻自动执行
     componentWillMount () {
+        console.log('componetWillMount')
         document.removeEventListener("keydown",this.handleEnterKey);
     }
+    // 在组件被挂载到页面后 自动执行
+    componentDidMount () {
+        console.log('componentDidMount')
+        document.addEventListener("keydown",this.handleEnterKey);
+    }
+    // 组件被更新之前，自动执行
+    shouldComponentUpdate () {
+        console.log('shouldComponentUpdate')
+        return true;
+    }
+    // 组件被更新之前，会自动自动执行，
+    // 但在shouldComponentUpdate返回true之后执行
+    // 如果返回false，这个函数就不会被执行了
+    componentWillUpdate () {
+        console.log('componentWillUpdate')
+    }
+    // 组件更新完成之后，会自动执行
+    componentDidUpdate () {
+        console.log('componentDidUpdate') 
+    }
+    // 在接收props时，执行
+    componentWillReceiveProps () {
+
+    }
     handleEnterKey (e) {
-        console.log(e)
+        // console.log(e)
         if (e.keyCode === 13) {
             this.handleButtonClick()
         }
@@ -53,7 +79,8 @@ class TodoList extends Component {
         // this.setState({
         //     inputValue: e.target.value
         // });
-        const value = e.target.value
+        // const value = e.target.value
+        const value = this.input.value
         this.setState(() => {
             return {
                 inputValue: value
