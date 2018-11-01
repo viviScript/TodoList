f181789d03566b13ecffa3d4705306cdb78495a4
viviScript <863628979@qq.com>
2018-11-01 22:23:17 +0800

    redux改写

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+import store from './store';
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
+
     render () {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
                 <div>
-                    <Input style={{width: '300px', marginRight: '10px'}} placeholder='todolist input' />
-                    <Button type='primary'>提交</Button>
+                    <Input onChange={this.handleInputChange} value={this.state.inputValue} style={{width: '300px', marginRight: '10px'}} placeholder='todolist input' />
+                    <Button onClick={this.handleBtnClick} type='primary'>提交</Button>
                 </div>    
                 <List
                     style={{width: '300px',marginTop: '10px'}}
                     bordered
-                    dataSource={data}
+                    dataSource={this.state.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                     />
              </div>
         )
+    }
+    handleStoreChange () {
+        this.setState(store.getState());
+    }
+    handleInputChange (e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+    handleBtnClick () {
+        const action = {
+            type: 'add_list_item'
+        }
+        store.dispatch(action);
     }
 }
 
